0fed9f1b30c26c8549e70cd09a928066ba56009f
Damon Tian <damontian@live.com>
2016-11-30 17:33:08 -0500

    added mp3 forms via paperclip

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,5 +1,14 @@
 class Episode < ActiveRecord::Base
   belongs_to :podcast
+
   has_attached_file :thumbnail, :styles => { :large => "1000x1000#", :thumb => "500x500#" }
   validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
+
+  has_attached_file :mp3
+  validates_attachment_content_type :mp3, :content_type => [ 'application/octet-stream', 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
+
+  Paperclip.options[:content_type_mappings] = {
+  :mp3 => "application/octet-stream"
+	}
+
 end
