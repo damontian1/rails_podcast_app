d79fc86b4e8adbef2e432b76b965f0104fb39a42
Damon Tian <damontian@live.com>
2016-11-29 23:47:41 -0500

    added episode controller and views

---
+++ app/controllers/episodes_controller.rb
@@ -0,0 +1,40 @@
+class EpisodesController < ApplicationController
+	before_action :find_podcast
+	before_action :find_episode, only: [:show]
+
+	def show
+		@episodes = Episode.where(podcast_id: @podcast).order("created_at DESC").reject { |e| e.id == @episode.id }
+	end
+
+	def new
+		@episode = @podcast.episodes.build
+	end 
+
+	def create
+		@episode = @podcast.episodes.build(episode_params)
+
+		if @episode.save
+			redirect_to podcast_episode_path(@podcast, @episode)
+		else
+			render "new"
+		end
+	end
+
+	def destroy
+	end
+
+	private
+
+	def find_podcast
+		@podcast = Podcast.find(params[:podcast_id])
+	end
+
+	def find_episode
+		@episode = Episode.find(params[:id])
+	end
+
+	def episode_params
+		params.require(:episode).permit(:title, :description)
+	end
+
+end
